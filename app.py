cf0cbdb2f53a2185acf65108caebc7f012a8675b
egidocervera@gmail.com <egidocervera@gmail.com>
2025-07-24 20:18:53 +0200

    hello

--- app.py
+++ app.py
@@ -1,18 +1,6 @@
-y = int(input("Tell me any number you want: "))
-x = int(input("Tell me any other number you want: "))
+import functions
 
-def sum(a, b):
-    return a + b
+x = 4
 
 
-print(sum(x, y))
-
-
-
-z = int(input("Number: "))
-
-def factorial(a):
-    for i in range(1, a):
-        return i * (i + 1)
-
-print(factorial(z))
+print(functions.sqrt(x))
