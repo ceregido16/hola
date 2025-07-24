963fd4f00027fd86898dd1b489d8c9e8388db38e
egidocervera@gmail.com <egidocervera@gmail.com>
2025-07-24 18:44:53 +0200

    added a main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+function sum(a, b) {
+    return a + b
+};
+
+let x = prompt("Tell me any number you want: ");
+let y = prompt("Tell me any number you want: ");
+
+y = parseInt(y);
+x = parseInt(x);
+
+console.log(sum(x, y));
